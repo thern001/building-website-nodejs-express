95d1bae96df807059a9170d0b9b7e66ecb3a041c
thern001 <thern001@gmail.com>
2021-10-16 19:44:59 -0400

    finished chapter 6

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,5 +1,6 @@
+const { request, response } = require('express');
 const express = require('express');
-const { NotExtended } = require('http-errors');
+const { check, validateResult, validationResult } = require('express-validator');
 
 const router = express.Router();
 
@@ -9,15 +10,47 @@ module.exports = (params) => {
   router.get('/', async (req, res, next) => {
     try {
       const feedback = await feedbackService.getList();
-      return res.render('layout', { template: 'feedback', pageTitle: 'Feedback', feedback });
+
+      const errors = req.session.feedback ? req.session.feedback.errors : false;
+      const successMessage = req.session.feedback ? req.session.feedback.message : false;
+      req.session.feedback = {};
+
+      return res.render('layout', {
+        template: 'feedback',
+        pageTitle: 'Feedback',
+        feedback,
+        errors,
+        successMessage,
+      });
     } catch (error) {
       return next(error);
     }
   });
 
-  router.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('Feedback form posted');
-  });
+  router.post(
+    '/',
+    [
+      check('name').trim().isLength({ min: 3 }).escape().withMessage('A name is required'),
+      check('email').trim().isEmail().normalizeEmail().withMessage('A valid email is required'),
+      check('title').trim().isLength({ min: 3 }).escape().withMessage('A title is required'),
+      check('message').trim().isLength({ min: 5 }).escape().withMessage('A message is required'),
+    ],
+    async (req, res) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        req.session.feedback = {
+          errors: errors.array(),
+        };
+        return res.redirect('/feedback');
+      }
+      const { name, email, title, message } = req.body;
+      await feedbackService.addEntry(name, email, title, message);
+      req.session.feedback = {
+        message: 'Thank you for your feedback!',
+      };
+      return res.redirect('/feedback');
+    }
+  );
   return router;
 };
